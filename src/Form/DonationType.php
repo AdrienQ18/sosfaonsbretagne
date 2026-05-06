66c636887f082cb2e1b9e1c5e24acb182f93686b
Adrien LE CLECH <alc.theclech@gmail.com>
2026-05-06 11:58:10 +0200

    06/05/2026 — résolution de 2 warnings ajout de lang="fr dans base.html.twig et suppression d'un import non utiliser

--- src/Form/DonationType.php
+++ src/Form/DonationType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use function Sodium\add;
 
 class DonationType extends AbstractType
 {

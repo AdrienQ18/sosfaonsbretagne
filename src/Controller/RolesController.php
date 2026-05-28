d56f11ec96ac2d8848c9e12f8176abf735deabff
Adrien LE CLECH <alc.theclech@gmail.com>
2026-05-28 11:36:26 +0200

    28/05/2026 — Ajout la possibilité dans Availability et dans Roles, pour un admin de modifier ou de supprimer.

--- src/Controller/RolesController.php
+++ src/Controller/RolesController.php
@@ -11,29 +11,61 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/roles')]
 final class RolesController extends AbstractController
 {
-    #[Route(name: 'admin_roles', methods: ['GET', 'POST'])]
+    #[Route('/admin/roles', name: 'admin_roles', methods: ['GET', 'POST'])]
+    #[Route('/admin/roles/update/{id}', name: 'admin_roles_update', methods: ['GET', 'POST'])]
     public function index(
         RoleRepository         $roleRepository,
         EntityManagerInterface $entityManager,
         Request                $request,
+        ?int                   $id = null,
     ): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        if ($id !== null) {
+            $role = $roleRepository->find($id);
+            if (!$role) {
+                throw $this->createNotFoundException('La disponibilité n\'existe pas');
+            }
+        } else {
+            $role = new Role();
+        }
         $roleList = $roleRepository->findAll();
-        $role = new Role();
         $formRole = $this->createForm(RoleType::class, $role);
         $formRole->handleRequest($request);
 
         if ($formRole->isSubmitted() && $formRole->isValid()) {
-            $entityManager->persist($role);
+            if ($id === null) {
+                $entityManager->persist($role);
+            }
             $entityManager->flush();
+            $this->addFlash('success', $id === null ? 'Rôle ajouté avec succès.' : 'Rôle modifié avec succès.');
             return $this->redirectToRoute('admin_roles');
         }
         return $this->render('roles/adminRole.html.twig', [
             'roleList' => $roleList,
             'formRole' => $formRole->createView(),
+            'isEdit' => $id !== null,
         ]);
     }
+
+    #[Route('/admin/roles/delete/{id}', name: 'admin_roles_delete', methods: ['POST'])]
+    public function deleteRole(
+        int                    $id,
+        RoleRepository         $roleRepository,
+        EntityManagerInterface $entityManager,
+    ): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $role = $roleRepository->find($id);
+        if (!$role) {
+            throw $this->createNotFoundException('La disponibilité n\'existe pas');
+        }
+        $entityManager->remove($role);
+        $entityManager->flush();
+        $this->addFlash('success', 'Votre rôle a bien été supprimé.');
+        return $this->redirectToRoute('admin_roles');
+    }
+
 }
